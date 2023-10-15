123d5ab9e342b078c65fb51ea72822983ede489d
Davin Ly <vinh.ly.softdev@gmail.com>
2023-10-16 01:54:37 +1030

    initial for React TS UI

---
+++ web/src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="bg-blue-500 text-white p-5">
+      Hello, this is a React app with TypeScript and Tailwind!
+    </div>
+  );
+}
+
+export default App;
